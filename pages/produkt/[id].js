cb439f78ea7b706009fbe034369cd2c855c4b1ac
Sebastian Roming <sebastian@webmonkey.io>
2023-09-05 16:04:37 +0200

    Test

---
+++ pages/produkt/[id].js
@@ -0,0 +1,9 @@
+export default function CollectionsPage(props){
+  return (
+  <p>Test</p>
+  )
+}
+
+export async function getServerSideProps({params,locale}){
+  return {props:{}}
+}
--- vercel-json.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "VERCEL: $VERCEL"
-
-if [[ "$VERCEL" == "1" ]] ; then
-  rm vercel.json
-  mv vercel-storybook.json vercel.json
-  exit 1;
-fi
